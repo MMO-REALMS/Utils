b9b2ef11732eb4dc264f2a80517dcc81f54d7a37
Radu Voinea <contact@raduvoinea.com>
2026-03-15 00:03:37 +0200

    Rename Exceptionambda interface to ExceptionLambda for improved clarity and consistency; bump version to 1.22.8

--- src/main/java/com/raduvoinea/utils/lambda/lambda/throwing/ExceptionLambda.java
+++ src/main/java/com/raduvoinea/utils/lambda/lambda/throwing/ExceptionLambda.java
@@ -1,6 +1,6 @@
 package com.raduvoinea.utils.lambda.lambda.throwing;
 
 @FunctionalInterface
-public interface Exceptionambda<ThrownException extends Exception>{
+public interface ExceptionLambda<ThrownException extends Exception>{
 	void run() throws ThrownException;
 }
