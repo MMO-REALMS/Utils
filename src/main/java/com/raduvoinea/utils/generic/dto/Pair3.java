0e2ed1ca3c67a26878e81959d5aa741e1ccc22f1
Radu Voinea <contact@raduvoinea.com>
2026-03-15 01:55:38 +0200

    Bump version to 1.22.9 and add static factory method to Pair3 for improved usability

--- src/main/java/com/raduvoinea/utils/generic/dto/Pair3.java
+++ src/main/java/com/raduvoinea/utils/generic/dto/Pair3.java
@@ -2,4 +2,8 @@ package com.raduvoinea.utils.generic.dto;
 
 public record Pair3<First, Second, Third>(First first, Second second, Third third) {
 
+	public static <First, Second, Third> Pair3<First, Second, Third> of(First first, Second second, Third third) {
+		return new Pair3<>(first, second, third);
+	}
+
 }
