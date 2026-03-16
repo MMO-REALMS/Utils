7e9025eaab96229a95a116a8708e7a2cdc660632
Radu Voinea <contact@raduvoinea.com>
2026-03-17 00:02:25 +0200

    Enhance ChildLoggerInstance to handle null parent cases and bump version to 1.23.7

--- src/main/java/com/raduvoinea/utils/logger/ChildLoggerInstance.java
+++ src/main/java/com/raduvoinea/utils/logger/ChildLoggerInstance.java
@@ -21,38 +21,58 @@ public class ChildLoggerInstance extends LoggerInstance {
 	}
 
 	protected void handleInfo(@NotNull String log) {
-		this.parent.handleInfo(log);
+		if (parent != null) {
+			this.parent.handleInfo(log);
+		}
 	}
 
 	protected void handleError(@NotNull String log) {
-		this.parent.handleError(log);
+		if (parent != null) {
+			this.parent.handleError(log);
+		}
 	}
 
 	protected void handleWarn(@NotNull String log) {
-		this.parent.handleWarn(log);
+		if (parent != null) {
+			this.parent.handleWarn(log);
+		}
 	}
 
 	protected void handleDebug(@NotNull String log) {
-		this.parent.handleDebug(log);
+		if (parent != null) {
+			this.parent.handleDebug(log);
+		}
 	}
 
 	protected String finalProcessor(String log) {
-		return this.parent.finalProcessor(log);
+		if (parent != null) {
+			return this.parent.finalProcessor(log);
+		}
+		return log;
 	}
 
 	protected @Nullable String parsePackage(String packageName) {
-		return this.parent.parsePackage(packageName);
+		if (parent != null) {
+			return this.parent.parsePackage(packageName);
+		}
+
+		return null;
 	}
 
 	@Override
 	public void setPrintSourceClass(boolean printSourceClass) {
 		super.setPrintSourceClass(printSourceClass);
-		this.parent.setPrintSourceClass(printSourceClass);
+		if (parent != null) {
+
+			this.parent.setPrintSourceClass(printSourceClass);
+		}
 	}
 
 	@Override
 	public void setLogLevel(Level logLevel) {
 		super.setLogLevel(logLevel);
-		this.parent.setLogLevel(logLevel);
+		if (parent != null) {
+			this.parent.setLogLevel(logLevel);
+		}
 	}
 }
